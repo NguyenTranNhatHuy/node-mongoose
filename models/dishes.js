f5654712ab10c89c2fda550244324bc28456fac7
NhatHuy <ntnh2762002@gmail.com>
2024-01-18 13:49:23 +0700

    first commit

---
+++ models/dishes.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Scheme = mongoose.Schema;
+
+
+var commentSchema = new Scheme({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+var dishSchema = new Scheme({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    comments: [commentSchema]
+}, {
+    timestamps: true
+});
+
+var Dishes = mongoose.model('Dish', dishSchema);
+module.exports = Dishes;
